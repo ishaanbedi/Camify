1a7ea8f38612db6bb97973763a0fd224e1b45c97
Ishaan Bedi <bediishaan6@gmail.com>
2023-03-06 13:55:13 +0530

    Upload Project to Github

---
+++ Camify/TransactionsView.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionsView.swift
+//  Camify
+//
+//  Created by Ishaan Bedi on 06/03/23.
+//
+
+import SwiftUI
+
+struct TransactionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TransactionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TransactionsView()
+    }
+}
