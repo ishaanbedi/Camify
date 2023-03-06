518ee6fb6cde9f28d9109be09cf3711435e00119
Ishaan Bedi <bediishaan6@gmail.com>
2023-03-06 07:35:17 +0530

    Initial Commit

---
+++ Camify/CamifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamifyApp.swift
+//  Camify
+//
+//  Created by Ishaan Bedi on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CamifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
